612a3c499040bf32cda380d566cda53eac8a798b
Ephrem W <ephremworkeye@gmail.com>
2022-01-18 14:40:40 -0800

    fetched data to Home

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    
-  </div>
+
   <router-view/>
 </template>
 
 <style>
+body {
+  background: rgb(194, 197, 198);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #050a0e;
+  max-width: 600px;
+  margin: 0 auto;
+
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="home">
-  
+  <div class="home" >
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -10,6 +15,17 @@
 
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
